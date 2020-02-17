cf168867d30633e5c8ce1a3702a58e7c8a6e8b67
Jose Manuel Henche Ruiz <josemanuel.hruiz@gmail.com>
2020-02-17 14:06:41 +0100

    layout index

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,40 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+let allButtons = [...document.querySelectorAll('.options')]
+let buttonClickedArr = []
+
+function buttonClicked(allButtons) {
+  
+  allButtons.forEach((button) => {
+    button.addEventListener('click', function () {
+      button.classList.add('clicked')
+    
+    })
+      
+  })
+
+
+  
+
+}
+
+
+function firstSave() {
+  buttonClicked(allButtons)
+  document.querySelector('#save').addEventListener('click', function () {
+    
+    let btnArr = document.querySelectorAll('.clicked')
+    btnArr.forEach((btn) => {
+        buttonClickedArr.push(btn.value)
+    })
+
+    
+    console.log(buttonClickedArr)
+  })
+}
+
+firstSave()
+
+
