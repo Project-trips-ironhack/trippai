2293bf7a940b57135bd5e87eec8a2e8043ccc72f
Jose Manuel Henche Ruiz <josemanuel.hruiz@gmail.com>
2020-02-17 14:33:43 +0100

    FilterLayaoutWorcking

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,38 +5,62 @@ document.addEventListener('DOMContentLoaded', () => {
 }, false);
 
 let allButtons = [...document.querySelectorAll('.options')]
+let daysButtons = [...document.querySelectorAll('.days')]
+let dolarButtons = [...document.querySelectorAll('.dolar')]
+let typeButtons = [...document.querySelectorAll('.type')]
 let buttonClickedArr = []
 
-function buttonClicked(allButtons) {
-  
-  allButtons.forEach((button) => {
+function buttonClicked(daysButtons, dolarButtons, typeButtons) {
+  daysButtons.forEach((button, idx, arr) => {
+
     button.addEventListener('click', function () {
+      daysButtons.forEach((button) => {
+        button.classList.remove('clicked')
+      })
       button.classList.add('clicked')
-    
+
     })
-      
   })
+  dolarButtons.forEach((button) => {
+    button.addEventListener('click', function () {
+      dolarButtons.forEach((button) => {
+        button.classList.remove('clicked')
+      })
+      button.classList.add('clicked')
 
-
-  
-
+    })
+  })
+  typeButtons.forEach((button) => {
+    button.addEventListener('click', function () {
+      if (button.classList.contains('clicked')) {
+        button.classList.remove('clicked')
+      } else {
+        button.classList.add('clicked')
+      }
+    })
+  })
 }
 
+function removeClicked() {
+  allButtons.forEach(button => {
+    button.classList.remove('clicked')
+  })
+
+}
 
 function firstSave() {
-  buttonClicked(allButtons)
+  buttonClicked(daysButtons, dolarButtons, typeButtons)
   document.querySelector('#save').addEventListener('click', function () {
-    
     let btnArr = document.querySelectorAll('.clicked')
     btnArr.forEach((btn) => {
-        buttonClickedArr.push(btn.value)
+      buttonClickedArr.push(btn.value)
     })
-
-    
     console.log(buttonClickedArr)
+
+    removeClicked()
   })
 }
 
+
+
 firstSave()
-
-
