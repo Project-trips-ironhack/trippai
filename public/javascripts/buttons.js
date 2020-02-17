b93a84b92a87907b6fc5a9be13beae066ba2cc25
Arturo G. Bruno <arturogbruno@gmail.com>
2020-02-17 18:24:42 +0100

    file rename

---
+++ public/javascripts/buttons.js
@@ -0,0 +1,73 @@
+let allButtons = [...document.querySelectorAll('.options')]
+let daysButtons = [...document.querySelectorAll('.days')]
+let dolarButtons = [...document.querySelectorAll('.dolar')]
+let tagsButtons = [...document.querySelectorAll('.tags')]
+let buttonClickedDayArr = ''
+let buttonClickedDolarArr = ''
+let buttonClickedTagsArr = []
+let buttonNonClickedTagArr = []
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('IronGenerator JS imported successfully!');
+}, false);
+window.onload = function () {
+  tagsButtons.forEach((button) => {
+    button.classList.add('nonClickedTags')
+  })
+}
+daysButtons.forEach((button, idx, arr) => {
+  button.addEventListener('click', function (event) {
+    event.preventDefault()
+    daysButtons.forEach((button) => {
+      button.classList.remove('clickedDays')
+    })
+    button.classList.add('clickedDays')
+  })
+})
+dolarButtons.forEach((button) => {
+  button.addEventListener('click', function (event) {
+    event.preventDefault()
+    dolarButtons.forEach((button) => {
+      button.classList.remove('clickedDolar')
+    })
+    button.classList.add('clickedDolar')
+  })
+})
+tagsButtons.forEach((button, idx) => {
+  button.addEventListener('click', function (event) {
+    event.preventDefault()
+    button.classList.toggle('nonClickedTags')
+    button.classList.toggle('clickedTags')
+  })
+})
+function removeClicked() {
+  allButtons.forEach(button => {
+    button.classList.remove('clickedTags', 'clickedButtons', 'clickedDays', 'nonClickedTags')
+  })
+  buttonClickedArr = []
+  buttonClickedTagsArr = []
+}
+document.querySelector('#save').addEventListener('click', function () {
+  let btnArrDays = [...document.querySelectorAll('.clickedDays')]
+  let btnArrDolar = [...document.querySelectorAll('.clickedDolar')]
+  let btnArrTags = [...document.querySelectorAll('.clickedTags')]
+  let btnArrTagsNonClicked = [...document.querySelectorAll('.nonClickedTags')]
+  btnArrTagsNonClicked.forEach((btn) => {
+    buttonNonClickedTagArr.push(btn.value)
+  })
+  btnArrDays.forEach((btn) => {
+    buttonClickedDayArr = +btn.value
+  })
+  btnArrDolar.forEach((btn) => {
+    buttonClickedDolarArr = btn.value
+  })
+  btnArrTags.forEach((btn) => {
+    buttonClickedTagsArr.push(btn.value)
+  })
+  
+  document.getElementById('days').value = buttonClickedDayArr
+  document.getElementById('budget').value = buttonClickedDolarArr
+  document.getElementById('tagsWanted').value = buttonClickedTagsArr
+  document.getElementById('tagsNotWanted').value = buttonNonClickedTagArr
+  removeClicked()
+})
--- routes/search.js
+++ routes/search.js
@@ -6,11 +6,18 @@ const Day = require("../models/Day");
 const Travel = require("../models/Travel");
 
 router.post('/', (req, res, next) => {
+    const { days, budget, tagsWanted, tagsNotWanted } = req.body;
+    let arrTagsWanted = tagsWanted.split(',')
+    let arrTagsNotWanted = tagsNotWanted.split(',')
+    let numberDays = +days
+    console.log(days)
+    console.log(budget)
+    console.log(arrTagsWanted)
+    console.log(arrTagsNotWanted)
 
-    const {days, budget, tagsWanted, tagsNotWanted} = req.body;
-
-    Travel.find({$and: [ {days: {$size: days}}, {budget: budget}, {tags: {$all: tagsWanted}}, {tags: {$nin: tagsNotWanted}} ] })
-    .then(data => res.render('cities', {data}))
+    Travel.find({$and: [ {days: {$size: numberDays}}, {budget: budget}, {tags: {$all: arrTagsWanted}}, {tags: {$nin: arrTagsNotWanted}} ] })
+    // .then(data => res.render('cities', {data}))
+    .then(data => res.json(data))
     .catch(err => console.log(err));
 });
 
