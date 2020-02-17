8f8aa5a65863009d98ba3d089749f3bf8eba0430
Arturo G. Bruno <arturogbruno@gmail.com>
2020-02-17 15:45:36 +0100

    add create plan button to navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  const currentUser = req.user;
+  res.render('index', {currentUser});
 });
 
 module.exports = router;
