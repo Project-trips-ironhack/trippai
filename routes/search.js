8e9b9a15e1321183d1caacc41580e0a1508e60aa
Jose Manuel Henche Ruiz <josemanuel.hruiz@gmail.com>
2020-02-18 10:43:37 +0100

    layoutPlansDone

--- routes/search.js
+++ routes/search.js
@@ -5,15 +5,13 @@ const City = require("../models/City");
 const Day = require("../models/Day");
 const Travel = require("../models/Travel");
 
+
+
 router.post('/', (req, res, next) => {
     const { days, budget, tagsWanted, tagsNotWanted } = req.body;
     let arrTagsWanted = tagsWanted.split(',')
     let arrTagsNotWanted = tagsNotWanted.split(',')
     let numberDays = +days
-    console.log(days)
-    console.log(budget)
-    console.log(arrTagsWanted)
-    console.log(arrTagsNotWanted)
 
     Travel.find({$and: [ {days: {$size: numberDays}}, {budget: budget}, {tags: {$all: arrTagsWanted}}, {tags: {$nin: arrTagsNotWanted}} ] })
         .populate('city')
